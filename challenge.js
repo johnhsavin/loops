d6f840c4071164ec1a76dafbda84983acd5b25b8
John Savin <johnsavin@proton.me>
2023-10-10 10:52:35 -0400

    loops demo

---
+++ challenge.js
@@ -0,0 +1,6 @@
+const videos = ['cats', 'news', 'sports']
+//Console log each of these videos to the screen
+//Make sure it works for any length of the array
+for (let i = 0; i < videos.length; i++){
+    console.log(videos[i])
+}
---
+++ loops.js
@@ -0,0 +1,22 @@
+// Say hello 3x
+for (let i = 0; i < 3 ;i++){
+    console.log('hello')
+}
+
+/*
+0
+1
+2
+3
+*/
+for ( let i = 0; i < 4; i++){
+    console.log(i)
+}
+
+
+const people = ['todd', 'andrew', 'derek']
+// make a loop that says hello to each person in the array
+for(let i = 0; i < people.length; i++){
+    console.log('hello ' + people[i])
+}
+
